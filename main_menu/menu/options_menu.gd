c10caf64d85a6b75de0a4c69ee70831fd4dc3a15
Fok3d <sklenarm.06@spst.eu>
2024-11-07 18:02:58 +0100

    resolution done

--- main_menu/menu/options_menu.gd
+++ main_menu/menu/options_menu.gd
@@ -13,7 +13,13 @@ func _on_volume_value_changed(value: float) -> void:
 
 
 func _on_resolution_item_selected(index: int) -> void:
-	pass # Replace with function body.
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1920,1080))
+		1:
+			DisplayServer.window_set_size(Vector2i(1600,900))
+		2:
+			DisplayServer.window_set_size(Vector2i(1280,720))
 
 
 func _on_mute_toggled(toggled_on: bool) -> void:
