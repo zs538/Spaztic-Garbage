e8a4e71181362e878c94f886b35a1579cfa1f91b
Fok3d <sklenarm.06@spst.eu>
2024-11-07 17:43:52 +0100

    smooth criminal

--- main_menu/menu/options_menu.gd
+++ main_menu/menu/options_menu.gd
@@ -8,4 +8,14 @@ func _on_back_pressed() -> void:
 
 
 func _on_volume_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(0,value)
+	AudioServer.set_bus_volume_db(0,value/5)
+
+
+func _on_toggled_button_pressed():
+	
+	if self.pressed:
+		
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("AudioStreamPlayer"), true)
+	else:
+		
+		AudioServer.set_bus_mute(AudioServer.get_bus_index("AudioStreamPlayer"), false)
